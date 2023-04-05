536ee2d51df162b01573c6177ad6c0c0a5cc323b
Lasha gorakudashvili <lashatamo11@gmail.com>
2023-04-05 22:53:59 +0400

    done

---
+++ app.js
@@ -0,0 +1,26 @@
+const  myComputerManufacturer= "my computer is Manufacturer by intel";
+console.log(typeof  myComputerManufacturer,  myComputerManufacturer);
+
+const myComputerMemory = "my computer has 8 gigabytes of ram";
+console.log(typeof myComputerMemory, myComputerMemory);
+
+const myComputerOS= "my computer runs on windows 10";
+console.log(typeof myComputerOS, myComputerOS);
+
+const computerBuyingYear= "i bought my computer in 2022";
+console.log(typeof computerBuyingYear, computerBuyingYear);
+
+const currentYear = "im still using my computer in 2023";
+console.log(typeof currentYear, currentYear);
+
+const timeofuse = "and I've been using it for";
+console.log(typeof timeofuse, timeofuse);
+
+let num1 = "2022";
+let num2 = 2023;
+
+const substr =num2 - num1;
+console.log(substr, "num2 - num1");
+
+const year = "year";
+console.log(typeof year, year);
