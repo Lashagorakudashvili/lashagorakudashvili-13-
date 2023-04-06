85526ec233a3b9c18263718e72affd3baa396621
Lasha gorakudashvili <lashatamo11@gmail.com>
2023-04-06 15:09:23 +0400

    update

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const  myComputerManufacturer= "my computer is Manufacturer by intel";
-console.log(typeof  myComputerManufacturer,  myComputerManufacturer);
+console.log(myComputerManufacturer);
 
 const myComputerMemory = "my computer has 8 gigabytes of ram";
-console.log(typeof myComputerMemory, myComputerMemory);
+console.log(myComputerMemory);
 
 const myComputerOS= "my computer runs on windows 10";
-console.log(typeof myComputerOS, myComputerOS);
+console.log(myComputerOS);
 
 const computerBuyingYear= "i bought my computer in 2022";
-console.log(typeof computerBuyingYear, computerBuyingYear);
+console.log(computerBuyingYear);
 
 const currentYear = "im still using my computer in 2023";
-console.log(typeof currentYear, currentYear);
+console.log(currentYear);
 
 const timeofuse = "and I've been using it for";
-console.log(typeof timeofuse, timeofuse);
+console.log(timeofuse);
 
 let num1 = "2022";
 let num2 = 2023;
@@ -23,4 +23,4 @@ const substr =num2 - num1;
 console.log(substr, "num2 - num1");
 
 const year = "year";
-console.log(typeof year, year);
+console.log(year);
